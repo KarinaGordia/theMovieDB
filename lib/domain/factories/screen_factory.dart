cfdaa33be7155558628bbff46e8d49dea353a123
KarinaGordia <kgordya24@gmail.com>
2024-12-16 16:33:51 +0100

    83_mvvm fill screen factory

--- lib/domain/factories/screen_factory.dart
+++ lib/domain/factories/screen_factory.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:the_movie_db/library/widgets/inherited/provider.dart' as old_provider;
 import 'package:the_movie_db/ui/widgets/widgets.dart';
 
 class ScreenFactory {
@@ -9,5 +10,32 @@ class ScreenFactory {
       lazy: false,
       child: const LoaderWidget(),
     );
+  }
+
+  Widget makeAuth() {
+    return old_provider.NotifierProvider(
+      create: () => AuthorizationPageModel(),
+      child: const AuthorizationPage(),
+    );
+  }
+
+  Widget makeMain() {
+    return old_provider.NotifierProvider(
+      create: () => MainPageModel(),
+      child: const MainPage(),
+    );
+  }
+
+  Widget makeMovieDetails(int movieId) {
+    return old_provider.NotifierProvider(
+      create: () => MovieDetailsModel(movieId),
+      child: const MovieDetailsWidget(
+        primaryColor: Color.fromRGBO(10, 31, 52, 1),
+      ),
+    );
+  }
+
+  Widget makeMovieTrailer(String youTubeKey) {
+    return MovieTrailerWidget(youTubeKey: youTubeKey);
   }
 }
